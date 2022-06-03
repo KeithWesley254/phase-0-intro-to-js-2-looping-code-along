6f9ab399722e0395730831f19afd1872c85d6ae4
Keith <keithwesley187@gmail.com>
2022-06-03 18:35:50 +0300

    commit tree d3ceb2faa0d2231efebba4b387536dc9c9c8ea54

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(names, events){
+    const newmessage=[];
+
+    for(let i = 0; i < names.length; i++){
+    const thankyoumessage=`Thank you, ${names[i]}, for the wonderful ${events} gift!`;
+    newmessage.push(thankyoumessage);
+    }
+
+    return newmessage
+}
+console.log(writeCards([names],events))
+
+const numbers= []
+
+function countDown(numbers){
+    let i=10;
+    while(i>=0){
+    console.log(i--);
+    }
+    return (numbers);
+}
+console.log(countDown())
